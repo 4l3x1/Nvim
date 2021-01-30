d5f621178aff5181688a552fee3479787d198bd9
4l3x1 <38837038+4l3x1@users.noreply.github.com>
2021-01-30 10:10:17 -0500

    Update hh.py

--- hh.py
+++ hh.py
@@ -1,30 +1,18 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Servo
+from time import sleep
+ 
+myGPIO=17
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
-
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+servo = Servo(myGPIO)
+print("Rassberry Pi Servo"); 
+while True:
+    servo.min()
+    print("min")
+    sleep(0.5)
+    servo.mid()
+    print("mid")
+    sleep(1)
+    servo.max()
+    print("max")
+   
+    sleep(1)
