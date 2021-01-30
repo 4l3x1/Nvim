72a2f380e489d63ba314f5b700e29efdc027bde0
4l3x1 <38837038+4l3x1@users.noreply.github.com>
2021-01-30 11:48:00 -0500

    Update hh.py

--- hh.py
+++ hh.py
@@ -1,18 +1,44 @@
-from gpiozero import Servo
-from time import sleep
- 
-myGPIO=17
+#!/usr/bin/env python3
+#-- coding: utf-8 --
+import RPi.GPIO as GPIO
+import time
 
-servo = Servo(myGPIO)
-print("Rassberry Pi Servo"); 
-while True:
-    servo.min()
-    print("min")
-    sleep(0.5)
-    servo.mid()
-    print("mid")
-    sleep(1)
-    servo.max()
-    print("max")
-   
-    sleep(1)
+
+#Set function to calculate percent from angle
+def angle_to_percent (angle) :
+    if angle > 180 or angle < 0 :
+        return False
+
+    start = 4
+    end = 12.5
+    ratio = (end - start)/180 #Calcul ratio from angle to percent
+
+    angle_as_percent = angle * ratio
+
+    return start + angle_as_percent
+
+
+GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
+GPIO.setwarnings(False) #Disable warnings
+
+#Use pin 12 for PWM signal
+pwm_gpio = 12
+frequence = 50
+GPIO.setup(pwm_gpio, GPIO.OUT)
+pwm = GPIO.PWM(pwm_gpio, frequence)
+
+#Init at 0°
+pwm.start(angle_to_percent(0))
+time.sleep(1)
+
+#Go at 90°
+pwm.ChangeDutyCycle(angle_to_percent(90))
+time.sleep(1)
+
+#Finish at 180°
+pwm.ChangeDutyCycle(angle_to_percent(180))
+time.sleep(1)
+
+#Close GPIO & cleanup
+pwm.stop()
+GPIO.cleanup()
